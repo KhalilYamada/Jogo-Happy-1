977c45bfcb3833140a7a7494e5f7ba2be0b6e31f
Khalil Yamada HC <khalil.yamada@hotmailcom>
2020-03-27 20:11:06 -0300

    Jogo infinite runner até o momento
    Jogo infinite runner até o momento

---
+++ mod 2/Infinite Runner/Assets/Scripts/ScenarioCollider.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScenarioCollider : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D coll)
+    {
+        if(coll.CompareTag("object") || coll.CompareTag("item"))
+        {
+            Destroy(coll.gameObject);
+        }
+    }
+}
